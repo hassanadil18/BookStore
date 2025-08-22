2c5c5acd5273f0f642ab52cff0b1d64aaed0641e
hassanadil18 <hassanadilha2@gmail.com>
2025-08-22 11:01:56 +0500

    Update DB config to use MYSQL_URL

--- config/db.js
+++ config/db.js
@@ -1,25 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,      
-    process.env.DB_USER,      
-    process.env.DB_PASSWORD,  
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-);
+  }
+});
 
 sequelize.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch(err => console.log('DB connection error:', err));
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('DB connection error:', err));
 
 module.exports = sequelize;
--- models/index.js
+++ models/index.js
@@ -5,20 +5,14 @@ const Book = require('./book');
 User.initModel(sequelize);
 Book.initModel(sequelize);
 
-User.initModel(sequelize);
-Book.initModel(sequelize);
-
 const syncDatabase = async () => {
   try {
-    await sequelize.authenticate();
-    console.log('Database connection has been established successfully.');
-    
     if (process.env.NODE_ENV !== 'production') {
       await sequelize.sync({ alter: true });
       console.log('Database & tables synced');
     }
   } catch (err) {
-    console.error('Unable to connect to the database:', err);
+    console.error('Unable to sync database:', err);
   }
 };
 
